c979af0a64f7c5cac4ad59541a0addf7664d8ffb
Cowchie <gigliotticole@gmail.com>
2023-06-01 17:26:58 -0400

    Tested a map-type thing for root labeling

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <optional>
+#include <functional>
 
 class A{
 public:
@@ -19,12 +20,57 @@ public:
 //     return b.val;
 // }
 
+// Testing an O(RANK) compile-time implementation of map
+// This works specificaly for my use-case. But I don't like it...
+
+template<int n, char gen(int)>
+struct const_map{
+private:
+    char keys[n]  = { 0 };
+private:
+    inline constexpr void instantiate_keys(){
+        for (int i = 0; i < n; i++)
+            keys[i] = gen(i);
+    }
+public:
+    constexpr const_map(){
+        instantiate_keys();
+    }
+    constexpr std::optional<int> operator[](char k) const{
+        for (int i = 0; i < n; i++){
+            if (keys[i] == k){
+                return i;
+            }
+        }
+        return std::nullopt;
+    }
+    constexpr size_t size() const{
+        return n;
+    }
+};
+
+constexpr char f(int index){ return '1' + index; }; 
+
 int main()
 {
     std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    constexpr auto map_test = const_map<7, f>();
+    std::cout << map_test.size() << std::endl;
+
+    std::cout << map_test['1'].value_or(-1) << std::endl;
+    std::cout << map_test['2'].value_or(-1) << std::endl;
+    std::cout << map_test['3'].value_or(-1) << std::endl;
+    std::cout << map_test['4'].value_or(-1) << std::endl;
+    std::cout << map_test['5'].value_or(-1) << std::endl;
+    std::cout << map_test['6'].value_or(-1) << std::endl;
+    std::cout << map_test['7'].value_or(-1) << std::endl;
+    std::cout << map_test['8'].value_or(-1) << std::endl;
 
     for (const std::string& word : msg){
         std::cout << word << " ";
     }
     std::cout << std::endl;
+
+    return 0;
 }
